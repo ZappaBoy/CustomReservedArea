efb6b59d0c982534422032768192264c2cebdf17
zappaboy <federico_zappone@hotmail.it>
2020-02-24 23:30:20 +0100

    Added old working parts and vendor user role check

--- index.php
+++ index.php
@@ -8,19 +8,16 @@
  * Author URI: https://molise-italia.it
  */
 
-add_shortcode("user_page", "ciao" );
-
-function ciao($attr) {
+add_shortcode("user_page", "main" );
+function main($attr) {
 	global $current_user;
-	
-	
 	$editable_post_id = get_user_meta( $current_user->ID, 'editable_posts');
 	
 	$settings = array(
         'post_id'       => $editable_post_id[0],
         'post_title'    => false,
         'post_content'  => false,
-        'submit_value'  => __('Update meta')
+        'submit_value'  => __('Modifica')
     );
 	
 	echo '<div id="primary" class="content-area">
@@ -37,5 +34,21 @@ function ciao($attr) {
 </div><!-- #primary -->';
 }
 
+add_shortcode("preview-button", "preview_button_shortcode" );
+function preview_button_shortcode($attr) {
+	global $current_user;
+	$editable_post_id = get_user_meta( $current_user->ID, 'editable_posts');
+	
+	return do_shortcode('[fusion_button link="'. get_permalink($editable_post_id[0]) . '" text_transform="" title="" target="_self" link_attributes="" alignment="center" modal="" hide_on_mobile="small-visibility,medium-visibility,large-visibility" class="" id="" color="darkgreen" button_gradient_top_color="" button_gradient_bottom_color="" button_gradient_top_color_hover="" button_gradient_bottom_color_hover="" accent_color="" accent_hover_color="" type="" bevel_color="" border_width="" border_radius="" border_color="" border_hover_color="" size="xlarge" stretch="default" icon="fa-home fas" icon_position="left" icon_divider="no" animation_type="" animation_direction="left" animation_speed="0.3" animation_offset=""]Visualizza tua azienda[/fusion_button]');
+}
 
+add_shortcode("vendor-button", "vendor_button_shortcode" );
+function vendor_button_shortcode($attr) {
+	
+	$user = wp_get_current_user();
+	
+	if ( in_array( 'wcfm_vendor', (array) $user->roles ) ) {
+		return do_shortcode('[fusion_button link="/manager-negozio/" text_transform="" title="" target="_self" link_attributes="" alignment="center" modal="" hide_on_mobile="small-visibility,medium-visibility,large-visibility" class="" id="" color="darkgreen" button_gradient_top_color="" button_gradient_bottom_color="" button_gradient_top_color_hover="" button_gradient_bottom_color_hover="" accent_color="" accent_hover_color="" type="" bevel_color="" border_width="" border_radius="" border_color="" border_hover_color="" size="xlarge" stretch="default" icon="fa-store-alt fas" icon_position="left" icon_divider="no" animation_type="" animation_direction="left" animation_speed="0.3" animation_offset=""]Il tuo negozio[/fusion_button]');
+	}
+}
 
